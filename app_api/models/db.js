f308aa92013d0cd47ebb9e613bfa5bfc23588e67
Haydarozlukk <haydarozlukk@gmail.com>
2022-11-21 23:48:32 +0300

    Gönderimmm

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,4 +1,4 @@
-var dbURI="mongodb+srv://Haydar:mekanbul123@mekanbul.eoxzoum.mongodb.net/mekanbul?retryWrites=true&w=majority";
+var dbURI="mongodb+srv://Haydar:1234@mekanbul.juxcr0h.mongodb.net/?retryWrites=true&w=majorit";
 var mongoose=require("mongoose");
 //var dbURI = 'mongodb://127.0.0.1/mekanbul'; 
 mongoose.connect(dbURI);
