8e853685e362d580d763dd73d77158f7b4de8170
Haydarozlukk <haydarozlukk@gmail.com>
2022-11-21 23:53:56 +0300

    gönderim

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,4 +1,4 @@
-var dbURI="mongodb+srv://Haydar:1234@mekanbul.juxcr0h.mongodb.net/?retryWrites=true&w=majorit";
+var dbURI="mongodb+srv://Haydar:1234@mekanbul.juxcr0h.mongodb.net/mekanbul?retryWrites=true&w=majorit";
 var mongoose=require("mongoose");
 //var dbURI = 'mongodb://127.0.0.1/mekanbul'; 
 mongoose.connect(dbURI);
