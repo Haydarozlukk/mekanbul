c7691b98e58558186e961af68aa6107744bf5a00
Haydarozlukk <haydarozlukk@gmail.com>
2022-11-22 14:14:26 +0300

    3

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,39 +1,35 @@
+var mongoose = require('mongoose');
+//var dbURI = 'mongodb://localhost/mekanbul';
+var dbURI = "mongodb+srv://Haydar:1234@mekanbul.n1nfimq.mongodb.net/?retryWrites=true&w=majority";
+require("./mekansema");
 
-var mongoose=require("mongoose");
-var dbURI="mongodb+srv://Haydar:1234@mekanbul.fgp9txa.mongodb.net/mekanbul?retryWrites=true&w=majority"
-//var dbURI = 'mongodb://127.0.0.1/mekanbul'; 
-mongoose.connect(dbURI);
+
 function kapat(msg,callback){
     mongoose.connection.close(function(){
         console.log(msg);
         callback();
+    });
 }
 
-);
-
-}
 process.on("SIGINT",function(){
     kapat("Uygulama kapatıldı!",function(){
-            process.exit(0);
-    }
-    );
-}
-);
-mongoose.connection.on("connected",function(){
-console.log(dbURI+" adresindeki veritabanına bağlanıldı!");
-}
+        process.exit(0);
+    })
+})
 
 
-
-
-);
-mongoose.connection.on("disconnected",function(){
-    console.log(dbURI+" adresindeki veritabanı bağlantısı koptu");
-}
-
-);
-mongoose.connection.on("error",function(){
-    console.log("Bağlantı hatası");
-}
-);
-require("./mekansema"); 
+mongoose.connect(dbURI);
+mongoose.connection.on(
+    'connected', function(){
+        console.log(dbURI + " adresindeki veritabanına bağlanıldı!\n");
+    });
+    
+mongoose.connection.on(
+    'error', function(){
+        console.log(dbURI + "Bağlantı hatası!\n");
+    });
+    
+mongoose.connection.on(
+    'disconnected', function(){
+        console.log(dbURI + "Bağlantı koptu!\n");
+    });
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,27 +1,27 @@
-var express = require('express');
-var router = express.Router();
-var ctrlMekanlar=require('../controllers/mekanlar');
-var ctrlYorumlar=require('../controllers/yorumlar');
+var express=require("express");
+var router=express.Router();
+var ctrlMekanlar = require("../controllers/mekanlar");
+var ctrlYorumlar = require("../controllers/yorumlar");
 
 router
-.route('/mekanlar')
-.get(ctrlMekanlar.mekanlariListele)
-.post(ctrlMekanlar.mekanEkle);
-
-router
-.route('/mekanlar/:mekanid')
+.route("/mekanlar/:mekanid")
 .get(ctrlMekanlar.mekanGetir)
 .put(ctrlMekanlar.mekanGuncelle)
 .delete(ctrlMekanlar.mekanSil);
 
 router
-.route('/mekanlar/:mekanid/yorumlar')
+.route("/mekanlar")
+.get(ctrlMekanlar.mekanlariListele)
+.post(ctrlMekanlar.mekanEkle);
+
+router
+.route("/mekanlar/:mekanid/yorumlar")
 .post(ctrlYorumlar.yorumEkle);
 
 router
-.route('/mekanlar/:mekanid/yorumlar/:yorumid')
+.route("/mekanlar/:mekanid/yorumlar/:yorumid")
 .get(ctrlYorumlar.yorumGetir)
 .put(ctrlYorumlar.yorumGuncelle)
 .delete(ctrlYorumlar.yorumSil);
 
-module.exports=router;
+module.exports=router; //routeri dış dünyaya açıyoruz
