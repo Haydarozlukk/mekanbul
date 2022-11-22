785a65263f19542a951777acd1542f8068c302b0
Haydarozlukk <haydarozlukk@gmail.com>
2022-11-22 13:57:44 +0300

    Guncellss

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -3,34 +3,37 @@ var mongoose=require("mongoose");
 var dbURI="mongodb+srv://Haydar:1234@mekanbul.fgp9txa.mongodb.net/?retryWrites=true&w=majority";
 //var dbURI = 'mongodb://127.0.0.1/mekanbul'; 
 mongoose.connect(dbURI);
-
 function kapat(msg,callback){
     mongoose.connection.close(function(){
         console.log(msg);
         callback();
-    });
 }
 
+);
+
+}
 process.on("SIGINT",function(){
-    kapat("Uygulama kapatıldı",function(){
-        process.exit(0);
-    });
-})
-
-
+    kapat("Uygulama kapatıldı!",function(){
+            process.exit(0);
+    }
+    );
+}
+);
 mongoose.connection.on("connected",function(){
-    console.log(dbURI+"adresindeki veritabanına bağlandı.");
-})
+console.log(dbURI+" adresindeki veritabanına bağlanıldı!");
+}
 
 
+
+
+);
 mongoose.connection.on("disconnected",function(){
-    console.log("Bağlantı koptu.");
-})
+    console.log(dbURI+" adresindeki veritabanı bağlantısı koptu");
+}
 
-
+);
 mongoose.connection.on("error",function(){
-    console.log("Bağlantı hatası.");
-})
-
-
-require("./mekansema")
+    console.log("Bağlantı hatası");
+}
+);
+require("./mekansema");
